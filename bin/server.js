fdf569493950df7cc8add6b37e34b28a83f97998
Eugen Konkov <kes-kes@yandex.ru>
2015-09-14 15:33:34 +0300

    Logging to console

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,8 @@
 var http = require("http");
 
 function onRequest (request, response) {
+	console.log( 'Request received.' );
+
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello World");
 	response.end();
@@ -8,3 +10,5 @@ function onRequest (request, response) {
 
 var server =  http.createServer( onRequest );
 server.listen(8888);
+
+console.log( 'Server has started.' );
