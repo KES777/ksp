70b97465d379ddf7924beb76466cdf3b5b9fcc84
Eugen Konkov <kes-kes@yandex.ru>
2015-09-14 15:17:58 +0300

    Refactoring
    Give name for anonimous function

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,10 @@
 var http = require("http");
 
-http.createServer(
-	function(request, response) {
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello World");
-		response.end();
-	}
-).listen(8888);
+function onRequest (request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World");
+	response.end();
+}
+
+var server =  http.createServer( onRequest );
+server.listen(8888);
