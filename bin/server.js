5a5b8da98d9fe11b7e677fd75477e5fc237ba2e8
Eugen Konkov <kes-kes@yandex.ru>
2015-09-14 15:43:04 +0300

    Turn 'server.js' into module

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,18 @@
-var http = require("http");
+var http =  require("http");
 
-function onRequest (request, response) {
-	console.log( 'Request received.' );
+function start() {
+	function onRequest (request, response) {
+		console.log( 'Request received.' );
 
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World");
+		response.end();
+	}
+
+	var server =  http.createServer( onRequest );
+	server.listen(8888);
+
+	console.log( 'Server has started.' );
 }
 
-var server =  http.createServer( onRequest );
-server.listen(8888);
-
-console.log( 'Server has started.' );
+exports.start = start;
