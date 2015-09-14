c746794f6c2842521421960a4d1b93522f2f9c97
Eugen Konkov <kes-kes@yandex.ru>
2015-09-14 16:48:49 +0300

    Return meaningfull answer from handlers

--- bin/router.js
+++ bin/router.js
@@ -2,9 +2,11 @@ function route( pathname, handle ) {
 	console.log("About to route a request for " + pathname);
 
 	if (typeof handle[pathname] === 'function') {
-		handle[ pathname ]();
+		return handle[ pathname ]();
 	} else {
 		console.log("No request handler found for " + pathname);
+
+		return "404 Not found";
 	}
 }
 
--- bin/server.js
+++ bin/server.js
@@ -7,10 +7,10 @@ function start( route, handlers ) {
 		var pathname =  url.parse(request.url).pathname;
 		console.log( "Request for " + pathname + " received." );
 
-		route( pathname, handlers );
+		var content =  route( pathname, handlers );
 
 		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello World");
+		response.write( content );
 		response.end();
 		console.log( "Request for " +pathname + " processed." );
 	}
