4da8dfed68989ff9f26eae6a8b15c3f517e817dc
Eugen Konkov <kes-kes@yandex.ru>
2015-09-14 16:27:34 +0300

    Pass request handlers to server

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,10 @@
 var server_application =  require("./server");
 var router             =  require("./router");
+var requestHandlers    =  require("./requestHandlers");
 
-server_application.start( router.route );
+var handle          =  {};
+handle[ "/" ]       =  requestHandlers.start;
+handle[ "/start" ]  =  requestHandlers.start;
+handle[ "/upload" ] =  requestHandlers.upload;
+
+server_application.start( router.route, handle );
